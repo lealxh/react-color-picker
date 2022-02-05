899be5031df452e6ce5c69e5b5f418d406b1346b
Jose Leal <lealxh@gmail.com>
2022-02-05 10:05:31 -0300

    Cambiando los useState por ImmerReducer

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,10 +1,54 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { useImmerReducer } from "use-immer"
 import "./ColorPicker.css"
 
 function ColorPicker(props) {
-  const [getColors, setColors] = useState(props.Colors)
-  const [getSelectedColor, setSelectedColor] = useState(props.selectedColor)
-  const [getSelectedColorTag, setSelectedColorTag] = useState(props.selectedColor)
+  const InitialState = {
+    SelectedColor: {
+      hex: props.selectedColor,
+      r: "",
+      g: "",
+      b: ""
+    },
+    ColorButtons: props.Colors.map(colorValue => {
+      return {
+        isSelected: false,
+        color: {
+          hex: colorValue,
+          r: "",
+          g: "",
+          b: ""
+        },
+        style: { backgroundColor: colorValue }
+      }
+    })
+  }
+  function reducer(draft, action) {
+    switch (action.type) {
+      case "Click":
+        draft.SelectedColor = {
+          hex: action.value,
+          r: "",
+          g: "",
+          b: ""
+        }
+
+        for (let i = 0; i < draft.ColorButtons.length; i++) {
+          if (draft.ColorButtons[i].color.hex == draft.SelectedColor.hex) {
+            draft.ColorButtons[i].style = { backgroundColor: draft.ColorButtons[i].color.hex, boxShadow: draft.ColorButtons[i].color.hex + " 0px 0px 10px" }
+          } else {
+            draft.ColorButtons[i].style = { backgroundColor: draft.ColorButtons[i].color.hex }
+          }
+        }
+
+        return
+
+      case "TextEdit":
+        console.log("Input: " + action.value)
+        return
+    }
+  }
+  const [state, dispatch] = useImmerReducer(reducer, InitialState)
 
   function ColorToHex(color) {
     var hexadecimal = color.toString(16)
@@ -15,44 +59,44 @@ function ColorPicker(props) {
     return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue)
   }
 
-  function handleClick(e) {
-    const colorRGB = e.target.style.backgroundColor
-    const values = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/.exec(colorRGB)
-    console.log(ConvertRGBtoHex(values[1], values[2], values[3]))
-    setSelectedColorTag(ConvertRGBtoHex(parseInt(values[1]), parseInt(values[2]), parseInt(values[3])))
-    setSelectedColor(colorRGB)
-  }
-
   return (
     <div className="colorpicker">
       <div className="colorpicker-title">
         <div>Pick a color</div>
       </div>
-      <div className="canvas" style={{ backgroundColor: getSelectedColor }}>
-        <div className="colors-tag">{getSelectedColorTag}</div>
+      <div className="canvas" style={{ backgroundColor: state.SelectedColor.hex }}>
+        <div className="colors-tag">{state.SelectedColor.hex}</div>
       </div>
       <div className="controls-row">
         <div className="controls-column">
-          <input type="text" />
+          <input
+            value={state.SelectedColor.hex}
+            onChange={e => {
+              dispatch({ type: "TextEdit", value: e.target.value })
+            }}
+            id="hexField"
+            autoComplete="off"
+            type="text"
+          />
           <div className="controls-label">Hex</div>
         </div>
         <div className="controls-column">
-          <input type="text" />
+          <input id="rField" type="text" />
           <div className="controls-label">R</div>
         </div>
         <div className="controls-column">
-          <input type="text" />
+          <input id="gField" type="text" />
           <div className="controls-label">G</div>
         </div>
         <div className="controls-column">
-          <input type="text" />
+          <input id="bField" type="text" />
           <div className="controls-label">B</div>
         </div>
       </div>
 
       <div className="colors">
-        {getColors.map(colorValue => {
-          return <div key={colorValue} onClick={handleClick} className="colors-inner" style={{ backgroundColor: colorValue }}></div>
+        {state.ColorButtons.map(value => {
+          return <div key={value.color.hex} onClick={() => dispatch({ type: "Click", value: value.color.hex })} className="colors-inner" style={value.style}></div>
         })}
       </div>
     </div>
