375ceb62eb0d5fcbf64241e4249d08088cdecfa9
Jose Leal <lealxh@gmail.com>
2022-02-22 11:46:03 -0300

    modificando diseño

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,19 @@ import React, { useEffect } from "react"
 function About() {
   return (
     <>
-      <h1>About me</h1>
-      <p>Full stack programmer with more than 8 years of experience developing and maintaining web applications using frameworks such as .net C#, angular, react.js and web services (SOAP & API REST) for clients in the banking and investment area.</p>
-      <br />
+      <div className="about-me">
+        <h1>About me</h1>
+        <p>Full stack programmer with more than 8 years of experience developing and maintaining web applications using frameworks such as .net C#, angular, react.js and web services (SOAP & API REST) for clients in the banking and investment area.</p>
+        <br />
 
-      <a target={"_blank"} className="button" href="https://www.linkedin.com/in/jose-gregorio-leal-sanchez/?locale=en_US">
-        LinkedIn
-      </a>
-      <br />
-      <a target={"_blank"} className="button" href="https://github.com/lealxh/react-color-picker">
-        Github
-      </a>
+        <a target={"_blank"} className="button" href="https://www.linkedin.com/in/jose-gregorio-leal-sanchez/?locale=en_US">
+          LinkedIn
+        </a>
+        <br />
+        <a target={"_blank"} className="button" href="https://github.com/lealxh/react-color-picker">
+          Github
+        </a>
+      </div>
     </>
   )
 }
